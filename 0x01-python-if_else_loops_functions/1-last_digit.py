040791d3596de20280b688e6682784ba981b34bf
Ampong Boadu <ampsboadu@gmail.com>
2023-03-08 13:50:39 -0800

    print last digit correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1])
-digit = last_digit
+digit = int(str(number)[-1])
 
 if number < 0:
     digit *= -1
 
-if last_digit > 5:
+if digit > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, digit))
-elif last_digit > 0 and last_digit < 6:
+elif last_digit != 0 and last_digit < 6:
     text = "Last digit of {} is {} and is less than 6 and not 0"
     print(text.format(number, digit))
 else:
