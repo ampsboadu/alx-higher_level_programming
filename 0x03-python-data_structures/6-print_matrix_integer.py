65383461eeff6dc5b52b58f604f7a1e400bb3b88
Ampong Boadu <ampsboadu@gmail.com>
2023-03-11 23:10:24 -0800

    add tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,11 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         lght = len(row) - 1
+        i = 0
         while i <= lght:
             if i == lght:
                 print("{:d}".format(row[i]), end="")
             else:
                 print("{:d}".format(row[i]), end=" ")
+            i += 1
         print()
