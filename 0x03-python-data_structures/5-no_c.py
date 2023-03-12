fadc22da8c740663a90f99ba84697786016a64fc
Ampong Boadu <ampsboadu@gmail.com>
2023-03-11 22:48:55 -0800

    print matrix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    return ("".join(c for c in my_string if c not in "Cc"))
+    i = len(my_string) - 1
+    while i >= 0:
+        if (my_string[i] == 'C') or (my_string[i] == 'c'):
+            my_string = my_string[:i] + my_string[i + 1:]
+        i -= 1
+    return my_string
