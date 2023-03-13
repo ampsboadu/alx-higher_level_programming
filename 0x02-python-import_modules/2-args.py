67ff3dc3245fe60ff471eee45600f9bf3f231e48
Ampong Boadu <ampsboadu@gmail.com>
2023-03-12 22:33:14 -0700

    correction task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
-count = len(argv) - 1
-if count == 0:
-    print("{:d} arguments.".format(count))
-elif count == 1:
-    print("{:d} argument:".format(count))
-else:
-    print("{:d} arguments:".format(count))
+if __name__ == "__main__":
+    from sys import argv
+    count = len(argv) - 1
+    
+    if count == 0:
+        print("{:d} arguments.".format(count))
+    elif count == 1:
+        print("{:d} argument:".format(count))
+    else:
+        print("{:d} arguments:".format(count))
 
-i = 1;
-while i <= count:
-    print("{:d}: {:s}".format(i, argv[i]))
-    i += 1
+    i = 1;
+    while i <= count:
+        print("{:d}: {:s}".format(i, argv[i]))
+        i += 1
