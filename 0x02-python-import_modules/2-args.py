00b04f3e9d57193b35c45756cb8447e7ee9726ef
Ampong Boadu <ampsboadu@gmail.com>
2023-03-12 22:30:14 -0700

    task 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+count = len(argv) - 1
+if count == 0:
+    print("{:d} arguments.".format(count))
+elif count == 1:
+    print("{:d} argument:".format(count))
+else:
+    print("{:d} arguments:".format(count))
+
+i = 1;
+while i <= count:
+    print("{:d}: {:s}".format(i, argv[i]))
+    i += 1
