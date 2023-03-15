957ba898a7c1c855b7f60986a240109b8b89010f
Ampong Boadu <ampsboadu@gmail.com>
2023-03-15 14:18:31 -0700

    Higest value of dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     else:
-        #get first key
+        # get first key
         key1 = next(iter(a_dictionary))
         max = 0
         for key, value in a_dictionary.items():
