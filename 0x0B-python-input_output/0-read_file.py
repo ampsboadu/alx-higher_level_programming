8b6ad99257b8cff1de4af13ac8ba27fa486cd3c8
Ampong Boadu <ampsboadu@gmail.com>
2023-04-12 21:51:10 -0700

    task 0 correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Module 0-read_file
+
+Contains a function that read file and print content
+"""
+
 
 def read_file(filename=""):
+    """ Read file and print content """
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end="")
