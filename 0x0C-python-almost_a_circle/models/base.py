674db10e4ba6636a4149703aa6c8e0d8751a9074
Ampong Boadu <ampsboadu@gmail.com>
2023-04-17 12:23:11 -0700

    Base class created

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Module base contain a class Base
+
+"""
+
+class Base():
+    """
+    class Base definition
+
+    Attributes:
+        __nb_objects: int
+
+    Methods:
+        __init__(self, id=None)
+
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """set id or increment id if already set """
+        if id:
+            self.id = Base.__nb_objects
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
