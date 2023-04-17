75b0f14535a122f1c7d3cfdec0eed8266dc82c61
Ampong Boadu <ampsboadu@gmail.com>
2023-04-17 13:16:39 -0700

    Correction made to Setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -47,8 +47,8 @@ class Rectangle(Base):
         """width setter. value must be int greater than 0"""
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        elif value < 0:
-            raise ValueError('width must be >= 0')
+        elif value <= 0:
+            raise ValueError('width must be > 0')
         else:
             self.__width = value
 
@@ -62,8 +62,8 @@ class Rectangle(Base):
         """height setter. value must int greater than 0 """
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        elif value < 0:
-            raise ValueError('height must be >= 0')
+        elif value <= 0:
+            raise ValueError('height must be > 0')
         else:
             self.__height = value
 
