b4318d063555eb0051ba635e297e4f9a952eb7d3
Ampong Boadu <ampsboadu@gmail.com>
2023-04-18 20:51:44 -0700

    Save JSON to file added to Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,8 +29,20 @@ class Base():
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """return JSON obj representation"""
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string rep of objs to file"""
+        objs = []
+        if list_objs is not None:
+            for i in list_objs:
+                objs.append(cls.to_dictionary(i))
+
+        with open(cls.__name__ + ".json", 'w') as f:
+            f.write(cls.to_json_string(objs))
